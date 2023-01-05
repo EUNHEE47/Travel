bcd462c754d7e2e3c5440c86fdf7025a3c21a19c
박은희 <mii0329@naver.com>
2023-01-05 18:12:52 +0900

    sss

--- js/travel.js
+++ js/travel.js
@@ -1,15 +1,17 @@
+
+// main top 스크롤
 window.addEventListener('scroll', function(){
   // console.log(window.scrollY);
 
   if(window.scrollY >= 10){
-    document.querySelector('.main-top').style.background = 'black';
+    document.querySelector('.main-top').style.background = 'rgba(0, 0, 0, 0.8)';
   } else {
     document.querySelector('.main-top').style.background = 'none';
   }
 });
 
 
-
+// 프로모션
 let promotionCard = [
   {name:'SVC', memo:'항공사별 부가서비스'},
   {name:'KEOZUP', memo:'대한항공&아시아나항공 마일리지 승급 좌석 예약하기'},
@@ -32,11 +34,48 @@ for(let i=0; i<promotionCard.length; i++){
 };
 
 
-document.querySelector('#dep').addEventListener('click',function(){
-  document.querySelector('.dep-modal').style.display = 'block';
-  document.querySelector('body').style.background = 'black';
+// 항공 다구간
+$(".OJT").on('click',function(){
+  $("#select-OJT").toggle();
 });
 
-document.querySelector('.dep-modal').addEventListener('click',function(){
-  document.querySelector('.dep-modal').style.display = 'none';
+$(".RT").on('click',function(){
+  $("#select-OJT").css('display','none');
 });
+
+
+
+
+// 항공 검색창
+let depArrModal = document.querySelector(".dep-arr-modal");
+
+document.querySelector("#dep").addEventListener("click", function () {
+  depArrModal.style.display = "block";
+});
+
+document.querySelector("#arr").addEventListener("click", function () {
+  depArrModal.style.display = "block";
+});
+
+depArrModal.addEventListener("click", function () {
+  depArrModal.style.display = "none";
+});
+
+
+
+
+// 호텔 검색창
+let hotelModal = document.querySelector(".hotel-modal");
+
+document.querySelector("#hotel-select").addEventListener("click",function(){
+  hotelModal.style.display = 'block';
+});
+
+hotelModal.addEventListener('click',function(){
+  hotelModal.style.display = 'none';
+});
+
+document.querySelector('.hotel-modal-input').addEventListener('click', function(e){
+  e.preventDefault();
+});
+
